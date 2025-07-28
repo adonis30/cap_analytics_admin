124b5ba58ac1cb4126566f9e0eb45cd230ce5722
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-07-28 15:18:23 +0200

    Delete ZedMovies directory

--- ZedMovies/config/env.js
+++
@@ -1,5 +0,0 @@
-import { config } from "dotenv";
-
-config( {path: `.env.${process.env.NODE_ENV || "development"}.local`});
-
-export const {PORT, NODE_ENV, MONGO_URI, JWT_SECRET} = process.env;
--- ZedMovies/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-import { PORT } from './config/env.js';     
-
-const server = app.listen(PORT, () => {
-  console.log(`Zed Movies is running on http://localhost:${PORT}`);
-});
-
